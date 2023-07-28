d2245106550da6e0d17783ea803446aef4d82c93
justadude12 <72972938+justadude12@users.noreply.github.com>
2023-07-28 21:33:24 +0200

    The new updated version of the calculator features changes to its design, as now the button size and font size will deliberately change when the windowsize is adjusted. Furthermore, the textfield is now in readonly, as if it isn't, and it is selected, using the number keys will lead to adding a number twice.

--- Calculator_tk.py
+++ Calculator_tk.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import tkinter.font
 
 window = tk.Tk()
 window.title("Calculator")
@@ -6,7 +7,9 @@ window.eval('tk::PlaceWindow . center')
 text = tk.StringVar()
 text.set('')
 display = tk.Entry(window, textvariable = text)
-display.grid(row=0, column=0, columnspan=4)
+display.grid(row=0, column=0, sticky='nsew',columnspan=4)
+display.config(state='readonly')
+
 
 ####
 
@@ -43,7 +46,7 @@ def numberButton(x, text):
 
 buttons = []
 for i in range(10):
-    button = tk.Button(window, text=str(i), command=lambda: numberButton(str(i), text))
+    button = tk.Button(window, text=str(i), command=lambda i=i: numberButton(str(i), text))
     if(i != 0):
         button.grid(row=1 + (i - 1) // 3, column=(i - 1) % 3, sticky='nsew')
     else:
@@ -59,46 +62,44 @@ def actionButton(text, act):
     global canAction
     global action
     numbertxt = int(text.get())
-    if (action == Action.PLU):
-        val += numbertxt
-    elif (action == Action.MIN):
-        val -= numbertxt
-    elif (action == Action.TIM):
-        val *= numbertxt
-        val = int(val)
-    elif (action == Action.DIV):
-        if (numbertxt != 0):
-            val /= numbertxt
+    if (canAction):
+        if (action == Action.PLU):
+            val += numbertxt
+        elif (action == Action.MIN):
+            val -= numbertxt
+        elif (action == Action.TIM):
+            val *= numbertxt
             val = int(val)
+        elif (action == Action.DIV):
+            if (numbertxt != 0):
+                val /= numbertxt
+                val = int(val)
+            else:
+                text.set('Impossible to divide by 0!')
+        action = act
+        if (action != Action.CLE):
+            text.set(str(val))
         else:
-            text.set('Division par 0 impossible!')
-    action = act
-    if (action != Action.CLE):
-        text.set(str(val))
-    else:
-        text.set('')
-    if (action != Action.CLE):
-        if(action != Action.EQU):
-            canAction = False
+            text.set('')
+        if (action != Action.CLE):
+                canAction = False
         else:
-            canAction = True
-    else:
-        action = Action.PLU
+            action = Action.PLU
 
 ####
 
 buttonplu = tk.Button(window, text='+', command=lambda: actionButton(text, Action.PLU))
-buttonplu.grid(row=1, column=3)
+buttonplu.grid(row=1, column=3, sticky='nsew')
 buttonmin = tk.Button(window, text='-', command=lambda: actionButton(text, Action.MIN))
-buttonmin.grid(row=2, column=3)
+buttonmin.grid(row=2, column=3, sticky='nsew')
 buttoncle = tk.Button(window, text='C', command=lambda: actionButton(text, Action.CLE))
-buttoncle.grid(row=4, column=2)
+buttoncle.grid(row=4, column=2, sticky='nsew')
 buttontim = tk.Button(window, text='*', command=lambda: actionButton(text, Action.TIM))
-buttontim.grid(row=3, column=3)
+buttontim.grid(row=3, column=3, sticky='nsew')
 buttondiv = tk.Button(window, text='/', command=lambda: actionButton(text, Action.DIV))
-buttondiv.grid(row=4, column=1)
+buttondiv.grid(row=4, column=1, sticky='nsew')
 buttonequ = tk.Button(window, text='=', command=lambda: actionButton(text, Action.EQU))
-buttonequ.grid(row=4, column=3)
+buttonequ.grid(row=4, column=3, sticky='nsew')
 
 def num_key(event, num):
     numberButton(str(num), text)
@@ -130,7 +131,33 @@ def equal_key(event):
     buttonequ.invoke()
 window.bind('<Return>', equal_key)
 
+tk.Grid.rowconfigure(window, 0, weight=1)
+tk.Grid.rowconfigure(window, 1, weight=1)
+tk.Grid.rowconfigure(window, 2, weight=1)
+tk.Grid.rowconfigure(window, 3, weight=1)
+tk.Grid.rowconfigure(window, 4, weight=1)
+tk.Grid.columnconfigure(window, 0, weight=1)
+tk.Grid.columnconfigure(window, 1, weight=1)
+tk.Grid.columnconfigure(window, 2, weight=1)
+tk.Grid.columnconfigure(window, 3, weight=1)
+
+
 ####
+
+def resize(event):
+    # Calculate the new font size based on the Entry widget's height
+    new_size = -max(12, int(display.winfo_height() / 2))
+    display['font'] = tkinter.font.Font(size=new_size)
+    for button in buttons:
+        button['font'] = tkinter.font.Font(size=new_size)
+    buttonplu['font'] = tkinter.font.Font(size=new_size)
+    buttonmin['font'] = tkinter.font.Font(size=new_size)
+    buttoncle['font'] = tkinter.font.Font(size=new_size)
+    buttontim['font'] = tkinter.font.Font(size=new_size)
+    buttondiv['font'] = tkinter.font.Font(size=new_size)
+    buttonequ['font'] = tkinter.font.Font(size=new_size)
+window.bind('<Configure>', resize)
+
 
 # Handling the closing and opening of the window
 window.protocol('WM_DELETE_WINDOW', window.destroy)
